003563c52552e742164a3a56491a96a174148349
Matheus Magnusson <mathmagson@gmail.com>
2019-01-31 16:50:32 -0200

    Add encapsulamento

--- encapsulamento/exemplo-02.php
+++ encapsulamento/exemplo-02.php
@@ -20,6 +20,8 @@ class Programador extends Pessoa {
 
 	public function verDados(){
 
+		echo get_class($this) . "<br/>";
+
 		echo $this->nome . "<br/>";
 		echo $this->idade . "<br/>";
 		echo $this->senha . "<br/>";
