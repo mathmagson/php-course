5b3016634c20226192822640a31784e5810f1837
Matheus Magnusson <mathmagson@gmail.com>
2019-02-07 16:27:33 -0200

    Add DIR

---
+++ DIR/exemplo-02.php
@@ -0,0 +1,29 @@
+<?php 
+
+$images = scandir("images");
+
+//var_dump($images);
+
+$data = array();
+
+foreach ($images as $img) {
+	if (!in_array($img, array(".", ".."))) {
+
+		$filename = "images" . DIRECTORY_SEPARATOR . $img;
+
+		$info = pathinfo($filename);
+
+		$info["size"] = filesize($filename);
+		$info["modified"] = date("d/m/Y H:i:s", filemtime($filename));
+		$info["url"] = "http://localhost/DIR/" . str_replace("\\", "/", $filename);
+
+		//var_dump($info);
+
+		array_push($data, $info);
+
+	}
+}
+
+echo json_encode($data);
+
+ ?>
