1773fba5a9c7fd740964181e309decbc5df197ce
mathmagson <mathmagson@gmail.com>
2019-02-06 22:54:21 -0200

    Add DAO

--- DAO/index.php
+++ DAO/index.php
@@ -8,10 +8,40 @@ $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
 echo json_encode($usuarios);*/
 
-$user = new Usuario();
+// Carrega um usuário
+//$user = new Usuario();
+//$user->loadbyId(3);
+//echo $user;
 
-$user->loadbyId(3);
 
-echo $user;
+// Carrega uma lista de usuários
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+// Carrega uma lista de usuários buscando pelo login
+//$search = Usuario::search("jo");
+//echo json_encode($search);
+
+// Carrega um usuário usando o login e a senha
+//$usuario = new Usuario();
+//$usuario->login("joao", "jsrtus");
+//echo $usuario;
+
+// INSERT
+//$aluno = new Usuario("aluno", "@alun0");
+//$aluno->insert();
+//echo $aluno;
+
+// UPDATE
+//$usuario = new Usuario();
+//$usuario->loadById(8);
+//$usuario->update("professor", "1231");
+//echo $usuario;
+
+// DELETE
+$usuario = new Usuario();
+$usuario->loadById(7);
+$usuario->delete();
+echo $usuario;
 
  ?>
