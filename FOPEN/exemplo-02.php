b37fdabd9abdf26d044e1ab13a66ed80417b983a
mathmagson <mathmagson@gmail.com>
2019-02-10 17:48:07 -0200

    Add FOPEN

--- FOPEN/exemplo-02.php
+++ FOPEN/exemplo-02.php
@@ -6,6 +6,36 @@ $sql = new Sql();
 
 $usuarios = $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin");
 
-print_r($usuarios);
+//print_r($usuarios);
+
+$headers = array();
+
+foreach ($usuarios[0] as $key => $value) {
+	array_push($headers, ucfirst($key));
+}
+
+//print_r($headers);
+
+$file = fopen("usuarios.csv", "w+");
+
+fwrite($file, implode(",", $headers) . "\r\n");
+
+foreach ($usuarios as $row) {
+	
+	$data = array();
+
+	foreach ($row as $key => $value) {
+		
+		array_push($data, $value);
+
+	} // End foreach de coluna
+
+	fwrite($file, implode(",", $data) . "\r\n");
+
+} // End foreach de linha
+
+fclose($file);
+
+//echo implode(",", $headers);
 
  ?>
