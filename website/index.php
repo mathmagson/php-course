29989329d9947ae8b4c666a2d184c8644a44f5f3
Matheus Magnusson <mathmagson@gmail.com>
2019-02-11 13:26:02 -0200

    Add website

--- website/index.php
+++ website/index.php
@@ -1,5 +1,25 @@
 <?php 
 
-phpinfo();
+//phpinfo();
+
+require_once("vendor".DIRECTORY_SEPARATOR."autoload.php");
+
+$app = new \Slim\Slim();
+
+$app->get('/', function(){
+
+	//echo "Home Page";
+
+	echo json_encode(array(
+		'date'=>date("Y-n-d H:i:s")
+	));
+
+});
+
+$app->get('/hello/:name', function ($name) {
+    echo "Hello, " . $name;
+});
+
+$app->run();
 
  ?>
